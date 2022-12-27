7bc582d19d6a701e7d5b46eeb680166f80d6594a
chandrakant itagi <puc593813@gmail.com>
2022-12-27 10:37:23 +0000

    adding SampleApp/app/appcenter-pre-build.sh

---
+++ SampleApp/app/appcenter-pre-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# This updates the version number to be the build id from app center + the shift up to the build number from the google play store
+# Currently we don't shift the ios version number.
+echo "==================its pre build======================"
+PROJECT_NAME=Kidzo-andro
+
+ANDROID_GRADLE_FILE=$APPCENTER_SOURCE_DIRECTORY/SampleApp/app/build.gradle
+
+#INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/NameOfProjectOnAppCenter/ios/$PROJECT_NAME/Info.plist
+versionName=1.2.1
+VERSION_CODE=$APPCENTER_BUILD_ID
+
+echo "====$VERSION_CODE============"
+
+sed -i "" 's/versionName "[^"]*"/versionName "'$versionName.$VERSION_CODE'"/g' $ANDROID_GRADLE_FILE
+
