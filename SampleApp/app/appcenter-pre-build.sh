2151de7f5124f4dcd86761fffc0a1bf38fef599f
gmail il <ideagmal@gmail.com>
2023-01-03 06:57:32 +0000

    appcenter-pre-build.sh edited online with Bitbucket

--- SampleApp/app/appcenter-pre-build.sh
+++ SampleApp/app/appcenter-pre-build.sh
@@ -4,23 +4,19 @@ echo "==================Running pre build script======================"
 
 ANDROID_GRADLE_FILE=$APPCENTER_SOURCE_DIRECTORY/SampleApp/app/build.gradle
 
-echo "*****environment variable******"
-echo “VERSION=${VERSION}”
-
 
 versioninbuildgradle=`cat $ANDROID_GRADLE_FILE | grep versionName | awk '{print $2}' | tr -d \"`
-echo "******Versionname in build.gradle file"
+echo "******Versionname in build.gradle file*****"
 echo $versioninbuildgradle
-
-
-
-new_version=$VERSION.$APPCENTER_BUILD_ID
-
-#modifying the versionname with new_version in build.gradle file.
-sed -i "" 's/versionName "[^"]*"/versionName "'$new_version'"/g' $ANDROID_GRADLE_FILE
 
 #Using appcenter api's updating the VERSION environment variable in appcenter build configuration.
 
 export DATA=`curl -sX GET "https://api.appcenter.ms/v0.1/apps/testaccoutn/sampleapp/branches/master/config" -H "Content-Type: application/json" -H "X-API-Token: c105225fa414b9e5d5bcef925f400a5150af6610" |  jq '.environmentVariables += [{"name": "VERSION", "value":"'$versioninbuildgradle'"}]' -c`
 
 curl -X PUT -d "$DATA" "https://api.appcenter.ms/v0.1/apps/testaccoutn/sampleapp/branches/master/config" -H "Content-Type: application/json" -H "X-API-Token: c105225fa414b9e5d5bcef925f400a5150af6610"
+
+new_version=$VERSION.$APPCENTER_BUILD_ID
+
+#modifying the versionname with new_version in build.gradle file.
+sed -i "" 's/versionName "[^"]*"/versionName "'$new_version'"/g' $ANDROID_GRADLE_FILE
+
