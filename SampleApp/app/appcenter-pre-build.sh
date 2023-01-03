11ffa5b9aaf994633f01dcb2550537066c1d29bd
gmail il <ideagmal@gmail.com>
2023-01-03 06:12:29 +0000

    appcenter-pre-build.sh edited online with Bitbucket

--- SampleApp/app/appcenter-pre-build.sh
+++ SampleApp/app/appcenter-pre-build.sh
@@ -24,5 +24,3 @@ sed -i "" 's/versionName "[^"]*"/versionName "'$new_version'"/g' $ANDROID_GRADLE
 export DATA=`curl -sX GET "https://api.appcenter.ms/v0.1/apps/testaccoutn/sampleapp/branches/master/config" -H "Content-Type: application/json" -H "X-API-Token: c105225fa414b9e5d5bcef925f400a5150af6610" |  jq '.environmentVariables += [{"name": "VERSION", "value":"'$new_version'"}]' -c`
 
 curl -X PUT -d "$DATA" "https://api.appcenter.ms/v0.1/apps/testaccoutn/sampleapp/branches/master/config" -H "Content-Type: application/json" -H "X-API-Token: c105225fa414b9e5d5bcef925f400a5150af6610"
- 
-#curl -sX GET "https://api.appcenter.ms/v0.1/apps/testaccoutn/sampleapp/branches/master/config" -H "Content-Type: application/json" -H "X-API-Token: c105225fa414b9e5d5bcef925f400a5150af6610" | jq
